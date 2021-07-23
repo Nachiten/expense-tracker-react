9a2a5c66f8509fc2a035b6fcbac2d95c8e4c7b66
Ignacio Baptista [Notebook] <ibaptista@frba.utn.edu.ar>
2021-07-23 00:49:59 -0300

    Terminado Capitulo 43, An Alternative Function Syntax

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,4 @@
+import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
@@ -5,13 +6,13 @@ function ExpenseItem(props) {
    const expense = props.expense;
 
    return (
-      <div className="expense-item">
+      <Card className="expense-item">
          <ExpenseDate date={expense.date} />
          <div className="expense-item__description">
             <h2>{expense.title}</h2>
             <div className="expense-item__price">${expense.amount}</div>
          </div>
-      </div>
+      </Card>
    );
 }
 export default ExpenseItem;
