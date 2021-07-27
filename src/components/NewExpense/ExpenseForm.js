06ccf7cb79eb385f0bc9f6a6ae200bceb0a735c9
Ignacio Baptista [PC] <ibapista@frba.utn.edu.ar>
2021-07-27 16:49:44 -0300

    Terminado Tarea 4: Conditional Content

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,83 +1,128 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
 const ExpenseForm = (props) => {
-    const [userInput, setUserInput] = useState({
-        enteredTitle: "",
-        enteredDate: "",
-        enteredAmount: "",
-    });
+   const [userInput, setUserInput] = useState({
+      enteredTitle: "",
+      enteredDate: "",
+      enteredAmount: "",
+   });
 
-    const titleChangeHandler = (event) => {
-        setUserInput((prevState) => {
-            return {
-                ...prevState,
-                enteredTitle: event.target.value,
-            }
-        });
-    };
+   const [newExpenseShowed, setNewExpenseShowed] = useState(false);
 
-    const dateChangeHandler = (event) => {
-        setUserInput((prevState) => {
-            return {
-                ...prevState,
-                enteredDate: event.target.value,
-            }
-        });
-    };
+   const titleChangeHandler = (event) => {
+      setUserInput((prevState) => {
+         return {
+            ...prevState,
+            enteredTitle: event.target.value,
+         };
+      });
+   };
 
-    const amountChangeHandler = (event) => {
-        setUserInput((prevState) => {
-            return {
-                ...prevState,
-                enteredAmount: event.target.value,
-            }
-        });
-    };
+   const dateChangeHandler = (event) => {
+      setUserInput((prevState) => {
+         return {
+            ...prevState,
+            enteredDate: event.target.value,
+         };
+      });
+   };
 
-    const submitHandler = (event) => {
-        event.preventDefault();
+   const amountChangeHandler = (event) => {
+      setUserInput((prevState) => {
+         return {
+            ...prevState,
+            enteredAmount: event.target.value,
+         };
+      });
+   };
 
-        const expenseData = {
-            title: userInput.enteredTitle,
-            amount: userInput.enteredAmount,
-            date: new Date(userInput.enteredDate),
-        };
+   const submitHandler = (event) => {
+      setNewExpenseShowed(false);
 
-        props.onSaveExpenseData(expenseData);
+      event.preventDefault();
 
-        resetFormValues();
-    };
+      console.log(event.label);
 
-    const resetFormValues = () => {
-        setUserInput({
-            enteredTitle: "",
-            enteredDate: "",
-            enteredAmount: "",
-        });
-    };
+      const expenseData = {
+         title: userInput.enteredTitle,
+         amount: userInput.enteredAmount,
+         date: new Date(userInput.enteredDate),
+      };
 
-    return (
-        <form onSubmit={submitHandler}>
-            <div className="new-expense__controls">
-                <div className="new-expense__control">
-                    <label>Titulo</label>
-                    <input type="text" value={userInput.enteredTitle} onChange={titleChangeHandler} required />
-                </div>
-                <div className="new-expense__control">
-                    <label>Precio</label>
-                    <input type="number" value={userInput.enteredAmount} min="0.01" step="0.01" onChange={amountChangeHandler} required />
-                </div>
-                <div className="new-expense__control">
-                    <label>Fecha</label>
-                    <input type="date" value={userInput.enteredDate} min="2000-01-01" max="2023-12-31" onChange={dateChangeHandler} required />
-                </div>
+      props.onSaveExpenseData(expenseData);
+
+      resetFormValues();
+   };
+
+   const cancelHandler = () => {
+      setNewExpenseShowed(false);
+   }
+
+   const showNewExpense = () => {
+      setNewExpenseShowed(true);
+   }
+
+   const resetFormValues = () => {
+      setUserInput({
+         enteredTitle: "",
+         enteredDate: "",
+         enteredAmount: "",
+      });
+   };
+
+   if (!newExpenseShowed){
+      return( 
+      <div className="new-expense__controls">
+         <div className="new-expense__actions">
+         <button onClick={showNewExpense} className="agregar_gasto">Añadir Nuevo Gasto</button>
+         </div>
+      </div>
+      )
+   }
+
+   return (
+      <form onSubmit={submitHandler}>
+         <div className="new-expense__controls">
+            <div className="new-expense__control">
+               <label>Titulo</label>
+               <input
+                  type="text"
+                  value={userInput.enteredTitle}
+                  onChange={titleChangeHandler}
+                  required
+               />
             </div>
-            <div className="new-expense__actions">
-                <button type="submit">Añadir Gasto</button>
+            <div className="new-expense__control">
+               <label>Precio</label>
+               <input
+                  type="number"
+                  value={userInput.enteredAmount}
+                  min="0.01"
+                  step="0.01"
+                  onChange={amountChangeHandler}
+                  required
+               />
             </div>
-        </form>)
+            <div className="new-expense__control">
+               <label>Fecha</label>
+               <input
+                  type="date"
+                  value={userInput.enteredDate}
+                  min="2000-01-01"
+                  max="2023-12-31"
+                  onChange={dateChangeHandler}
+                  required
+               />
+            </div>
+         </div>
+         <div className="new-expense__actions">
+            <button onClick={cancelHandler}>Cancelar</button>
+            <button type="submit">Añadir Gasto</button>
+         </div>
+      </form>
+   );
 };
 
 export default ExpenseForm;
